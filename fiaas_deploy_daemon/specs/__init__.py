d0243862bbc276da06ef5abefce449ba84bbad8a
fimojoha <morten.lied.johansen@finn.no>
2016-08-29 11:38:04 +0200

    INFRA-947 - Binding for factories

--- fiaas_deploy_daemon/specs/__init__.py
+++ fiaas_deploy_daemon/specs/__init__.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8
 from __future__ import absolute_import
 
+import importlib
+import pkgutil
+
 import pinject
 
 from .factory import SpecFactory
@@ -10,6 +13,15 @@ from .factory import SpecFactory
 class SpecBindings(pinject.BindingSpec):
     def configure(self, bind):
         bind("spec_factory", to_class=SpecFactory)
+
+    def provide_factories(self):
+        factories = {}
+        for _, name, ispkg in pkgutil.iter_modules(__path__):
+            if ispkg and name.startswith("v"):
+                version = int(name[1])
+                module = importlib.import_module("{}.{}".format(__name__, name))
+                factories[version] = module.Factory()
+        return factories
 
 
 class InvalidConfiguration(Exception):

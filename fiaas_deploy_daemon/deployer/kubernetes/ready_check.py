a3bd66e2c491f54c582a8fccff308cc178f3690c
fimojoha <morten.lied.johansen@finn.no>
2016-10-13 13:28:44 +0200

    INFRA-1086 - Increase multiplier for fail-limit

--- fiaas_deploy_daemon/deployer/kubernetes/ready_check.py
+++ fiaas_deploy_daemon/deployer/kubernetes/ready_check.py
@@ -6,12 +6,14 @@ import time
 from k8s.models.deployment import Deployment
 from k8s.client import NotFound
 
+FAIL_LIMIT_MULTIPLIER = 1.5
+
 
 class ReadyCheck(object):
     def __init__(self, app_spec, bookkeeper):
         self._app_spec = app_spec
         self._bookkeeper = bookkeeper
-        fail_after_seconds = 1.1 * app_spec.replicas * app_spec.health_checks.readiness.initial_delay_seconds
+        fail_after_seconds = FAIL_LIMIT_MULTIPLIER * app_spec.replicas * app_spec.health_checks.readiness.initial_delay_seconds
         self._fail_after = time.time() + fail_after_seconds
 
     def __call__(self):

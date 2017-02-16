9f22e1d34bdd55ccf29bfcfd40776308d28a9d1e
Morten Lied Johansen <morten.lied.johansen@finn.no>
2017-02-16 10:49:45 +0100

    Count versions of fiaas.yml used (#73)

--- fiaas_deploy_daemon/specs/factory.py
+++ fiaas_deploy_daemon/specs/factory.py
@@ -3,12 +3,14 @@
 from __future__ import absolute_import
 
 import yaml
+from prometheus_client import Counter
 
 
 class SpecFactory(object):
     def __init__(self, session, factories):
         self._session = session
         self._factories = factories
+        self._fiaas_counter = Counter("fiaas_yml_version", "The version of fiaas.yml used", ["version"])
 
     def __call__(self, name, image, url, teams, tags):
         """Create an app_spec from the fiaas-config at the given URL"""
@@ -16,5 +18,6 @@ class SpecFactory(object):
         resp.raise_for_status()
         app_config = yaml.safe_load(resp.text)
         fiaas_version = app_config.get(u"version", 1)
+        self._fiaas_counter.labels(fiaas_version).inc()
         factory = self._factories[fiaas_version]
         return factory(name, image, teams, tags, app_config)

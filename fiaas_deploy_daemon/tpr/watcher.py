08383c54437644347fbb01d500e6f9c7776bfcd0
Øyvind Ingebrigtsen Øvergaard <oyvind.ingebrigtsen.overgaard@finn.no>
2017-05-10 13:21:46 +0200

    DOCD-936 Use watch_list to monitor PaasbetaApplication TPRs

--- fiaas_deploy_daemon/tpr/watcher.py
+++ fiaas_deploy_daemon/tpr/watcher.py
@@ -6,16 +6,16 @@ import time
 
 from ..base_thread import DaemonThread
 from .paasbetaapplication import PaasbetaApplication
+from k8s.base import WatchEvent
 
 LOG = logging.getLogger(__name__)
 
 
 class Watcher(DaemonThread):
-    def __init__(self, spec_factory, deploy_queue, poll_interval_seconds=1):
+    def __init__(self, spec_factory, deploy_queue):
         super(Watcher, self).__init__()
         self._spec_factory = spec_factory
         self._deploy_queue = deploy_queue
-        self._poll_interval_seconds = poll_interval_seconds
         # TODO: we rely on k8s client being configured in adapter.py. setup should be moved to fdd/__init__.py
 
     def is_alive(self):
@@ -24,13 +24,19 @@ class Watcher(DaemonThread):
     def __call__(self):
         while True:
             try:
-                time.sleep(self._poll_interval_seconds)
-
-                applications = PaasbetaApplication.list()
-                for application in applications:
-                    self._deploy(application)
+                for event in PaasbetaApplication.watch_list():
+                    self._handle_watch_event(event)
             except:
-                LOG.exception("Error while deploying or listing PaasbetaApplication")
+                LOG.exception("Error while watching for changes on PaasbetaApplications")
+
+    def _handle_watch_event(self, event):
+        if event.type in (WatchEvent.ADDED, WatchEvent.MODIFIED):
+            self._deploy(event.object)
+        elif event.type == WatchEvent.DELETED:
+            pass # TODO: this should delete the related kubernetes objects
+        else:
+            raise ValueError("Unknown WatchEvent type {}".format(event.type))
+
 
     def _deploy(self, application):
         LOG.debug("Deploying %s", application.spec.application)

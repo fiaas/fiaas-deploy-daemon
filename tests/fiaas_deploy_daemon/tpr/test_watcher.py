f35697c4624e3740dc7496cc16583a82e6f31c3e
Jose Armesto <github@armesto.net>
2017-05-18 14:20:02 +0200

    Improved readability of test

--- tests/fiaas_deploy_daemon/tpr/test_watcher.py
+++ tests/fiaas_deploy_daemon/tpr/test_watcher.py
@@ -10,27 +10,30 @@ from fiaas_deploy_daemon.tpr import Watcher
 from k8s.client import NotFound
 
 
+def given_third_party_resource_does_not_exist(mock_get):
+    mock_get.side_effect = NotFound("Something")
+
+
+def when_watching_the_third_party_resource():
+    watcher = Watcher(SpecFactory([]), Queue())
+    watcher._watch()
+
+
+def then_the_third_party_resource_should_be_created(mock_post):
+    mock_post.assert_called_once_with(
+        "/apis/extensions/v1beta1/thirdpartyresources/",
+        {
+            'metadata': {'namespace': 'default', 'name': 'paasbeta-application.schibsted.io'},
+            'description': 'A paas application definition',
+            'versions': [{'name': 'v1beta'}]
+        }
+    )
+
+
 class TestWatcher(object):
-    def given_third_party_resource_does_not_exist(self):
-        self.mock_get.side_effect = NotFound("Something")
-
-    def when_watching_the_third_party_resource(self):
-        self.watcher = Watcher(SpecFactory([]), Queue())
-        self.watcher._watch()
-
-    def then_the_third_party_resource_should_be_created(self):
-        self.mock_post.assert_called_once_with(
-            "/apis/extensions/v1beta1/thirdpartyresources/",
-            {
-                'metadata': {'namespace': 'default', 'name': 'paasbeta-application.schibsted.io'},
-                'description': 'A paas application definition',
-                'versions': [{'name': 'v1beta'}]
-            }
-        )
-
     def test_creates_third_party_resource_if_not_exists_when_tryi(self):
-        with mock.patch('k8s.client.Client.get') as self.mock_get, \
-                mock.patch('k8s.client.Client.post') as self.mock_post:
-            self.given_third_party_resource_does_not_exist()
-            self.when_watching_the_third_party_resource()
-            self.then_the_third_party_resource_should_be_created()
+        with mock.patch('k8s.client.Client.get') as mock_get, \
+                mock.patch('k8s.client.Client.post') as mock_post:
+            given_third_party_resource_does_not_exist(mock_get)
+            when_watching_the_third_party_resource()
+            then_the_third_party_resource_should_be_created(mock_post)

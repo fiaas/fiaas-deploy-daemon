0913f4c61a470d4fd113c05cbddf3f94d0d9e146
fimojoha <morten.lied.johansen@finn.no>
2017-06-01 15:39:29 +0200

    Log the name of the application before trying to parse fiaas.yml

--- fiaas_deploy_daemon/specs/factory.py
+++ fiaas_deploy_daemon/specs/factory.py
@@ -2,7 +2,12 @@
 # -*- coding: utf-8
 from __future__ import absolute_import
 
+import logging
+
 from prometheus_client import Counter
+
+
+LOG = logging.getLogger(__name__)
 
 
 class SpecFactory(object):
@@ -13,6 +18,7 @@ class SpecFactory(object):
     def __call__(self, name, image, app_config, teams, tags, deployment_id):
         """Create an app_spec from app_config"""
         fiaas_version = app_config.get(u"version", 1)
+        LOG.info("Creating app_spec for %s from fiaas.yml version %d", name, fiaas_version)
         self._fiaas_counter.labels(fiaas_version).inc()
         factory = self._factories[fiaas_version]
         return factory(name, image, teams, tags, app_config, deployment_id)

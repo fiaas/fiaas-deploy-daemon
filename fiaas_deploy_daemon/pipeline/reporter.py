dac8b57b75823662e335ad09a3795a4e1db9e6e8
Tollef Fog Heen <tollef.fog.heen@finn.no>
2017-09-19 14:11:13 +0200

    Log whether the call to pipeline succeeded or not

--- fiaas_deploy_daemon/pipeline/reporter.py
+++ fiaas_deploy_daemon/pipeline/reporter.py
@@ -3,6 +3,7 @@
 from __future__ import absolute_import
 
 import posixpath
+import logging
 from blinker import signal
 
 
@@ -13,6 +14,7 @@ class Reporter(object):
         self._infrastructure = config.infrastructure
         self._session = session
         self._callback_urls = {}
+        self._logger = logging.getLogger(__name__)
         signal("deploy_started").connect(self._handle_started)
         signal("deploy_success").connect(self._handle_success)
         signal("deploy_failed").connect(self._handle_failure)
@@ -31,7 +33,10 @@ class Reporter(object):
 
     def _handle_signal(self, event_name, app_spec, status=u"success"):
         base_url = self._callback_urls.get(app_spec.deployment_id)
-        if base_url:
-            task_name = u"fiaas_{}-{}_{}".format(self._environment, self._infrastructure, event_name)
-            url = posixpath.join(base_url, task_name, status)
-            self._session.post(url, json={u"description": u"From fiaas-deploy-daemon"})
+        if not base_url:
+            self._logger.info("No base URL for {} (deployment_id={}) found, not posting to pipeline".format(app_spec.name, app_spec.deployment_id))
+            return
+        task_name = u"fiaas_{}-{}_{}".format(self._environment, self._infrastructure, event_name)
+        url = posixpath.join(base_url, task_name, status)
+        r = self._session.post(url, json={u"description": u"From fiaas-deploy-daemon"})
+        self._logger.info("Posted {} to pipeline, return code={}".format(status, r.status_code))

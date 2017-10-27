bdb076067b02d307634acc644eddcf24050ae3ae
Øyvind Ingebrigtsen Øvergaard <oyvind.ingebrigtsen.overgaard@finn.no>
2017-10-27 11:02:41 +0200

    DOCD-1235 Use ingresses field on AppSpec to create ingress resource

--- fiaas_deploy_daemon/deployer/kubernetes/ingress.py
+++ fiaas_deploy_daemon/deployer/kubernetes/ingress.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8
 from __future__ import absolute_import
 
+from itertools import chain
 import logging
 
 from k8s.client import NotFound
@@ -36,28 +37,35 @@ class IngressDeployer(object):
     def _create(self, app_spec, labels):
         LOG.info("Creating/updating ingress for %s", app_spec.name)
         annotations = {
-            u"fiaas/expose": u"true" if app_spec.host else u"false"
+            u"fiaas/expose": u"true" if _has_explicitly_set_host(app_spec) else u"false"
         }
         custom_labels = merge_dicts(app_spec.labels.ingress, labels)
         custom_annotations = merge_dicts(app_spec.annotations.ingress, annotations)
         metadata = ObjectMeta(name=app_spec.name, namespace=app_spec.namespace, labels=custom_labels,
                               annotations=custom_annotations)
-        http_ingress_paths = [self._make_http_ingress_path(app_spec, port_spec) for port_spec in app_spec.ports if
-                              port_spec.protocol == u"http"]
-        http_ingress_rule = HTTPIngressRuleValue(paths=http_ingress_paths)
-        ingress_rules = [IngressRule(host=host, http=http_ingress_rule) for host in self._generate_hosts(app_spec)]
-        ingress_spec = IngressSpec(rules=ingress_rules)
+
+        per_host_ingress_rules = [IngressRule(host=self._apply_host_rewrite_rules(ingress_item.host),
+                                             http=self._make_http_ingress_rule_value(app_spec, ingress_item.pathmappings))
+                                  for ingress_item in app_spec.ingresses
+                                  if ingress_item.host is not None]
+        default_host_ingress_rules = self._create_default_host_ingress_rules(app_spec)
+
+        ingress_spec = IngressSpec(rules=per_host_ingress_rules + default_host_ingress_rules)
+
         ingress = Ingress.get_or_create(metadata=metadata, spec=ingress_spec)
         ingress.save()
 
-    def _generate_hosts(self, app_spec):
-        if app_spec.host:
-            yield self._make_ingress_host(app_spec)
+    def _generate_default_hosts(self, name):
         for suffix in self._ingress_suffixes:
-            yield u"{}.{}".format(app_spec.name, suffix)
+            yield u"{}.{}".format(name, suffix)
 
-    def _make_ingress_host(self, app_spec):
-        host = app_spec.host
+    def _create_default_host_ingress_rules(self, app_spec):
+        all_pathmappings = chain(*(ingress_item.pathmappings for ingress_item in app_spec.ingresses))
+        http_ingress_rule_value = self._make_http_ingress_rule_value(app_spec, all_pathmappings)
+        return [IngressRule(host=host, http=http_ingress_rule_value)
+                for host in self._generate_default_hosts(app_spec.name)]
+
+    def _apply_host_rewrite_rules(self, host):
         for rule in self._host_rewrite_rules:
             if rule.matches(host):
                 return rule.apply(host)
@@ -65,10 +73,30 @@ class IngressDeployer(object):
 
     @staticmethod
     def _should_have_ingress(app_spec):
-        return any(port.protocol == u"http" for port in app_spec.ports)
+        return len(app_spec.ingresses) > 0 and any(port.protocol == u"http" for port in app_spec.ports)
 
     @staticmethod
-    def _make_http_ingress_path(app_spec, port_spec):
-        backend = IngressBackend(serviceName=app_spec.name, servicePort=port_spec.port)
-        http_ingress_path = HTTPIngressPath(path=port_spec.path, backend=backend)
-        return http_ingress_path
+    def _make_http_ingress_rule_value(app_spec, pathmappings):
+        default_path = "/"
+        default_port = _get_default_port(app_spec)
+        if not default_port:
+            raise InvalidConfiguration("Could not find any ports with protocol=http!")
+
+        http_ingress_paths =[
+            HTTPIngressPath(path=pm.path if pm.path else default_path,
+                            backend=IngressBackend(serviceName=app_spec.name,
+                                                   servicePort=pm.port if pm.port else default_port))
+            for pm in pathmappings]
+
+        return HTTPIngressRuleValue(paths=http_ingress_paths)
+
+
+def _get_default_port(app_spec):
+    for port in app_spec.ports:
+        if port.protocol == u"http":
+            return port
+    return None
+
+
+def _has_explicitly_set_host(app_spec):
+    return any(ingress.host is not None for ingress in app_spec.ingresses)

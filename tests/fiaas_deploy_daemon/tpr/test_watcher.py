2e25b9a0919d76f835fb7a95e6e6cacf2e6d34c8
Øyvind Ingebrigtsen Øvergaard <oyvind.ingebrigtsen.overgaard@finn.no>
2017-11-09 14:25:45 +0100

    DOCD-1060 Use globally configured mocks in watcher test

--- tests/fiaas_deploy_daemon/tpr/test_watcher.py
+++ tests/fiaas_deploy_daemon/tpr/test_watcher.py
@@ -11,42 +11,31 @@ from fiaas_deploy_daemon.specs import SpecFactory
 from fiaas_deploy_daemon.specs.v2 import Factory
 from fiaas_deploy_daemon.tpr import Watcher
 
-
-def given_third_party_resource_does_not_exist(mock_get):
-    mock_get.side_effect = NotFound("Something")
-
-
-def when_watching_the_third_party_resource():
-    watcher = Watcher(SpecFactory(Factory(), {}), Queue())
-    watcher._watch()
-
-
-def then_the_third_party_resource_should_be_created(mock_post):
-    calls = [
-        mock.call("/apis/extensions/v1beta1/thirdpartyresources/", {
-            'metadata': {'namespace': 'default', 'name': 'paasbeta-application.schibsted.io'},
-            'description': 'A paas application definition',
-            'versions': [{'name': 'v1beta'}]
-        }),
-        mock.call("/apis/extensions/v1beta1/thirdpartyresources/", {
-            'metadata': {'namespace': 'default', 'name': 'paasbeta-status.schibsted.io'},
-            'description': 'A paas application status',
-            'versions': [{'name': 'v1beta'}]
-        })
-    ]
-    assert mock_post.call_args_list == calls
-
-
 class TestWatcher(object):
-    def test_creates_third_party_resource_if_not_exists_when_watching_it(self):
-        with mock.patch('k8s.client.Client.get') as mock_get, \
-                mock.patch('k8s.client.Client.post') as mock_post:
-            given_third_party_resource_does_not_exist(mock_get)
-            when_watching_the_third_party_resource()
-            then_the_third_party_resource_should_be_created(mock_post)
+    def test_creates_third_party_resource_if_not_exists_when_watching_it(self, get, post):
+        get.side_effect = NotFound("Something")
 
-    def test_is_able_to_watch_third_party_resource(self):
+        watcher = Watcher(SpecFactory(Factory(), {}), Queue())
+        watcher._watch()
+
+        calls = [
+            mock.call("/apis/extensions/v1beta1/thirdpartyresources/", {
+                'metadata': {'namespace': 'default', 'name': 'paasbeta-application.schibsted.io'},
+                'description': 'A paas application definition',
+                'versions': [{'name': 'v1beta'}]
+            }),
+            mock.call("/apis/extensions/v1beta1/thirdpartyresources/", {
+                'metadata': {'namespace': 'default', 'name': 'paasbeta-status.schibsted.io'},
+                'description': 'A paas application status',
+                'versions': [{'name': 'v1beta'}]
+            })
+        ]
+        assert post.call_args_list == calls
+
+    def test_is_able_to_watch_third_party_resource(self, get):
         response = Response()
+        get.return_value = response
+
         event = {
             'object': {
                 'metadata': {
@@ -70,9 +59,9 @@ class TestWatcher(object):
         }
         response.iter_content = Mock(return_value=[json.dumps(event)])
         response.status_code = Mock(return_value=200)
-        with mock.patch('k8s.client.Client.get', return_value=response):
-            watcher = Watcher(SpecFactory(Factory(), {}), Queue())
 
-            assert watcher._deploy_queue.qsize() == 0
-            watcher._watch()
-            assert watcher._deploy_queue.qsize() == 1
+        watcher = Watcher(SpecFactory(Factory(), {}), Queue())
+
+        assert watcher._deploy_queue.qsize() == 0
+        watcher._watch()
+        assert watcher._deploy_queue.qsize() == 1

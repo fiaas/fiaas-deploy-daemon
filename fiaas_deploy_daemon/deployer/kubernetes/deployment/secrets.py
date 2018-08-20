2a0cf229c843e4ab67937d08b2f157ad0069d36d
Morten Lied Johansen <mortenjo@ifi.uio.no>
2018-08-20 13:54:58 +0200

    Split secrets handling in three

--- fiaas_deploy_daemon/deployer/kubernetes/deployment/secrets.py
+++ fiaas_deploy_daemon/deployer/kubernetes/deployment/secrets.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
+
 from k8s.models.pod import EnvFromSource, SecretEnvSource, EnvVar, Container, ConfigMapEnvSource, VolumeMount, Volume, \
     EmptyDirVolumeSource, ConfigMapVolumeSource, SecretVolumeSource
 
@@ -7,6 +8,67 @@ from fiaas_deploy_daemon.tools import merge_dicts
 
 
 class Secrets(object):
+    def __init__(self, config):
+        self._secrets_image_set = config.secrets_init_container_image is not None
+        self._strongbox_image_set = config.strongbox_init_container_image is not None
+        self._generic_init = GenericInitSecrets(config)
+        self._strongbox = StrongboxSecrets(config)
+        self._kubernetes = KubernetesSecrets()
+
+    def _uses_strongbox_init_container(self, app_spec):
+        return self._strongbox_image_set and app_spec.strongbox.enabled
+
+    def apply(self, deployment, app_spec):
+        if self._secrets_image_set:
+            self._generic_init.apply(deployment, app_spec)
+        elif self._uses_strongbox_init_container(app_spec):
+            self._strongbox.apply(deployment, app_spec)
+        else:
+            self._kubernetes.apply(deployment, app_spec)
+
+
+class KubernetesSecrets(object):
+    def apply(self, deployment, app_spec):
+        deployment_spec = deployment.spec
+        pod_template_spec = deployment_spec.template
+        pod_spec = pod_template_spec.spec
+        main_container = pod_spec.containers[0]
+        env_from = main_container.envFrom
+
+        if app_spec.secrets_in_environment:
+            env_from.append(EnvFromSource(secretRef=SecretEnvSource(name=app_spec.name, optional=True)))
+
+        self._apply_mounts(app_spec, main_container)
+        self._apply_volumes(app_spec, pod_spec)
+
+    def _apply_volumes(self, app_spec, pod_spec):
+        volumes = []
+        volumes.extend(self._make_volumes(app_spec))
+        volumes.extend(pod_spec.volumes)
+        pod_spec.volumes = volumes
+
+    def _apply_mounts(self, app_spec, main_container):
+        volume_mounts = []
+        volume_mounts.extend(self._make_volume_mounts(app_spec, is_init_container=False))
+        volume_mounts.extend(main_container.volumeMounts)
+        main_container.volumeMounts = volume_mounts
+
+    def _make_volumes(self, app_spec):
+        volumes = [
+            Volume(name="{}-secret".format(app_spec.name),
+                   secret=SecretVolumeSource(secretName=app_spec.name, optional=True)),
+        ]
+        return volumes
+
+    def _make_volume_mounts(self, app_spec, is_init_container=False):
+        volume_mounts = [
+            VolumeMount(name="{}-secret".format(app_spec.name), readOnly=not is_init_container,
+                        mountPath="/var/run/secrets/fiaas/"),
+        ]
+        return volume_mounts
+
+
+class GenericInitSecrets(KubernetesSecrets):
     SECRETS_INIT_CONTAINER_NAME = "fiaas-secrets-init-container"
 
     def __init__(self, config):
@@ -20,44 +82,50 @@ class Secrets(object):
         pod_spec = pod_template_spec.spec
         main_container = pod_spec.containers[0]
         env_from = main_container.envFrom
+
+        # TODO: Replace with logged warning when fixing tests
         if app_spec.secrets_in_environment:
             env_from.append(EnvFromSource(secretRef=SecretEnvSource(name=app_spec.name, optional=True)))
 
-        volume_mounts = []
-        volume_mounts.extend(self._make_volume_mounts(app_spec, is_init_container=False))
-        volume_mounts.extend(main_container.volumeMounts)
-        main_container.volumeMounts = volume_mounts
+        self._apply_mounts(app_spec, main_container)
 
-        init_containers = pod_spec.initContainers
-        service_account_name = pod_spec.serviceAccountName
-        automount_service_account_token = pod_spec.automountServiceAccountToken
-        if self._uses_secrets_init_container():
-            init_container = self._make_secrets_init_container(app_spec, self._secrets_init_container_image)
-            init_containers.append(init_container)
-            automount_service_account_token = True
-            if self._secrets_service_account_name:
-                service_account_name = self._secrets_service_account_name
-        elif self._uses_strongbox_init_container(app_spec):
-            strongbox_env = {
-                "AWS_REGION": app_spec.strongbox.aws_region,
-                "SECRET_GROUPS": ",".join(app_spec.strongbox.groups),
-            }
-            init_container = self._make_secrets_init_container(app_spec, self._strongbox_init_container_image,
-                                                               env_vars=strongbox_env)
-            init_containers.append(init_container)
-        pod_spec.serviceAccountName = service_account_name
-        pod_spec.automountServiceAccountToken = automount_service_account_token
-        volumes = []
-        volumes.extend(self._make_volumes(app_spec))
-        volumes.extend(pod_spec.volumes)
-        pod_spec.volumes = volumes
+        init_container = self._make_secrets_init_container(app_spec, self._secrets_init_container_image)
+        pod_spec.initContainers.append(init_container)
+        if self._secrets_service_account_name:
+            pod_spec.serviceAccountName = self._secrets_service_account_name
+        pod_spec.automountServiceAccountToken = True
 
+        self._apply_volumes(app_spec, pod_spec)
+
+        # TODO: Push this down when fixing tests
         strongbox_annotations = self._make_strongbox_annotations(app_spec) if self._uses_strongbox_init_container(
             app_spec) else {}
         pod_metadata = pod_template_spec.metadata
         pod_metadata.annotations = merge_dicts(pod_metadata.annotations, strongbox_annotations)
 
-    def _make_secrets_init_container(self, app_spec, image, env_vars={}):
+    def _make_volumes(self, app_spec):
+        volumes = [
+            Volume(name="{}-secret".format(app_spec.name), emptyDir=EmptyDirVolumeSource()),
+            Volume(name="{}-config".format(self.SECRETS_INIT_CONTAINER_NAME),
+                   configMap=ConfigMapVolumeSource(name=self.SECRETS_INIT_CONTAINER_NAME, optional=True)),
+        ]
+        return volumes
+
+    def _make_volume_mounts(self, app_spec, is_init_container=False):
+        volume_mounts = super(GenericInitSecrets, self)._make_volume_mounts(app_spec, is_init_container)
+        if is_init_container:
+            init_container_mounts = [
+                VolumeMount(name="{}-config".format(self.SECRETS_INIT_CONTAINER_NAME), readOnly=True,
+                            mountPath="/var/run/config/{}/".format(self.SECRETS_INIT_CONTAINER_NAME)),
+                VolumeMount(name="{}-config".format(app_spec.name), readOnly=True, mountPath="/var/run/config/fiaas/"),
+                VolumeMount(name="tmp", readOnly=False, mountPath="/tmp"),
+            ]
+            volume_mounts.extend(init_container_mounts)
+        return volume_mounts
+
+    def _make_secrets_init_container(self, app_spec, image, env_vars=None):
+        if env_vars is None:
+            env_vars = {}
         env_vars.update({"K8S_DEPLOYMENT": app_spec.name})
         environment = [EnvVar(name=k, value=v) for k, v in env_vars.items()]
         container = Container(name=self.SECRETS_INIT_CONTAINER_NAME,
@@ -71,38 +139,39 @@ class Secrets(object):
                               volumeMounts=self._make_volume_mounts(app_spec, is_init_container=True))
         return container
 
-    def _make_volumes(self, app_spec):
-        volumes = []
-        if self._uses_secrets_init_container() or self._uses_strongbox_init_container(app_spec):
-            volumes.append(Volume(name="{}-secret".format(app_spec.name), emptyDir=EmptyDirVolumeSource()))
-            volumes.append(Volume(name="{}-config".format(self.SECRETS_INIT_CONTAINER_NAME),
-                                  configMap=ConfigMapVolumeSource(name=self.SECRETS_INIT_CONTAINER_NAME,
-                                                                  optional=True)))
-        else:
-            volumes.append(Volume(name="{}-secret".format(app_spec.name),
-                                  secret=SecretVolumeSource(secretName=app_spec.name, optional=True)))
-        return volumes
-
-    def _uses_secrets_init_container(self):
-        return bool(self._secrets_init_container_image)
-
+    # TODO: Push down when fixing tests
     def _uses_strongbox_init_container(self, app_spec):
         return self._strongbox_init_container_image is not None and app_spec.strongbox.enabled
-
-    def _make_volume_mounts(self, app_spec, is_init_container=False):
-        volume_mounts = []
-        volume_mounts.append(VolumeMount(name="{}-secret".format(app_spec.name),
-                                         readOnly=not is_init_container,
-                                         mountPath="/var/run/secrets/fiaas/"))
-        if is_init_container:
-            volume_mounts.append(VolumeMount(name="{}-config".format(self.SECRETS_INIT_CONTAINER_NAME),
-                                             readOnly=True,
-                                             mountPath="/var/run/config/{}/".format(self.SECRETS_INIT_CONTAINER_NAME)))
-            volume_mounts.append(
-                VolumeMount(name="{}-config".format(app_spec.name), readOnly=True, mountPath="/var/run/config/fiaas/"))
-            volume_mounts.append(VolumeMount(name="tmp", readOnly=False, mountPath="/tmp"))
-        return volume_mounts
 
     @staticmethod
     def _make_strongbox_annotations(app_spec):
         return {"iam.amazonaws.com/role": app_spec.strongbox.iam_role}
+
+
+class StrongboxSecrets(GenericInitSecrets):
+    def apply(self, deployment, app_spec):
+        deployment_spec = deployment.spec
+        pod_template_spec = deployment_spec.template
+        pod_spec = pod_template_spec.spec
+        main_container = pod_spec.containers[0]
+        env_from = main_container.envFrom
+
+        if app_spec.secrets_in_environment:
+            env_from.append(EnvFromSource(secretRef=SecretEnvSource(name=app_spec.name, optional=True)))
+
+        self._apply_mounts(app_spec, main_container)
+
+        strongbox_env = {
+            "AWS_REGION": app_spec.strongbox.aws_region,
+            "SECRET_GROUPS": ",".join(app_spec.strongbox.groups),
+        }
+        init_container = self._make_secrets_init_container(app_spec, self._strongbox_init_container_image,
+                                                           env_vars=strongbox_env)
+        pod_spec.initContainers.append(init_container)
+
+        self._apply_volumes(app_spec, pod_spec)
+
+        strongbox_annotations = self._make_strongbox_annotations(app_spec) if self._uses_strongbox_init_container(
+            app_spec) else {}
+        pod_metadata = pod_template_spec.metadata
+        pod_metadata.annotations = merge_dicts(pod_metadata.annotations, strongbox_annotations)

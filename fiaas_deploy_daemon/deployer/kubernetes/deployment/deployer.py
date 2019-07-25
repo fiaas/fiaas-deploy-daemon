accd15fb296ed2c2701db94cb35a5f956d735d12
Renato Arruda <renato.arruda@finn.no>
2019-07-25 16:19:36 +0200

    Improve logging for when configured replica size is ignored

--- fiaas_deploy_daemon/deployer/kubernetes/deployment/deployer.py
+++ fiaas_deploy_daemon/deployer/kubernetes/deployment/deployer.py
@@ -85,9 +85,9 @@ class DeploymentDeployer(object):
             try:
                 deployment = Deployment.get(app_spec.name, app_spec.namespace)
                 replicas = deployment.spec.replicas
+                LOG.warn("configured replica size (%d) for deployment is being ignored, as current running replica size"
+                         " is different (%d) for %s", deployment.spec.replicas, app_spec.replicas, app_spec.name)
             except NotFound:
-                LOG.warn("deployment not being scaled as autoscaler has been manually set to another value " +
-                         "for %s", app_spec.name)
                 pass
 
         deployment_strategy = DeploymentStrategy(

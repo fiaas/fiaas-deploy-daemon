76a8a8120207a95cb800aee61e80e9f7d7f8ccc0
Greg Jones <greg.jones@gmail.com>
2020-05-18 18:25:03 +0200

    Add OwnerReferences util
    This can be used to set the ownerReferences field on a k8s Model that
    has a 'metadata' field using the relevant information from an AppSpec.

---
+++ tests/fiaas_deploy_daemon/deployer/kubernetes/test_owner_references.py
@@ -0,0 +1,30 @@
+import pytest
+
+from k8s.models.common import ObjectMeta, OwnerReference
+from k8s.models.ingress import Ingress
+
+from fiaas_deploy_daemon.deployer.kubernetes.owner_references import OwnerReferences
+
+
+class TestOwnerReferences(object):
+
+    @pytest.fixture
+    def owner_references(self):
+        return OwnerReferences()
+
+    @pytest.fixture
+    def expected(self, app_spec):
+        expected = OwnerReference(apiVersion="fiaas.schibsted.io/v1",
+                                  kind="Application",
+                                  controller=True,
+                                  blockOwnerDeletion=True,
+                                  name=app_spec.name,
+                                  uid=app_spec.uid)
+        return expected
+
+    def test_apply(self, app_spec, owner_references, expected):
+        ingress = Ingress(metadata=ObjectMeta())
+
+        owner_references.apply(ingress, app_spec)
+
+        assert ingress.metadata.ownerReferences == [expected]

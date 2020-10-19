9e91f0b2d9a59b049f0b0607105c59ce895eb1ad
Sarhraoui Oussama <oussama.sarhraoui@schibsted.com>
2020-10-19 19:17:46 +0200

    Introduce a service account annotations and labels

--- tests/fiaas_deploy_daemon/deployer/kubernetes/test_deployment_deploy.py
+++ tests/fiaas_deploy_daemon/deployer/kubernetes/test_deployment_deploy.py
@@ -147,7 +147,7 @@ class TestDeploymentDeployer(object):
         generic_toggle, deploy_labels, deploy_annotations, pod_labels, pod_annotations, singleton = request.param
 
         labels = LabelAndAnnotationSpec(deployment=deploy_labels, horizontal_pod_autoscaler={}, ingress={},
-                                        service={}, service_account={}, pod=pod_labels, status={})
+                                        service={}, service_account={},pod=pod_labels, status={})
         annotations = LabelAndAnnotationSpec(deployment=deploy_annotations, horizontal_pod_autoscaler={}, ingress={},
                                              service={}, service_account={}, pod=pod_annotations, status={})
 

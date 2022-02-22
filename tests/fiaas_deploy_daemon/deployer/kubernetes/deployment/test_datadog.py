b6b07afb2ac9f9eb02817db6c086f03775d76185
eloy-maillo <eloy.maillo@adevinta.com>
2022-02-22 09:55:11 +0100

    Add a flag to activate the lifecycle in the datadog container

--- tests/fiaas_deploy_daemon/deployer/kubernetes/deployment/test_datadog.py
+++ tests/fiaas_deploy_daemon/deployer/kubernetes/deployment/test_datadog.py
@@ -136,8 +136,9 @@ class TestDataDog(object):
         assert actual['image'] == CONTAINER_IMAGE_LATEST
         assert actual['imagePullPolicy'] == "Always"
 
-    def test_adds_lifecycle_when_pre_stop_delay_is_set(self, config, app_spec, deployment):
+    def test_adds_lifecycle_when_pre_stop_delay_is_set_and_sleep_is_active(self, config, app_spec, deployment):
         config.datadog_container_image = CONTAINER_IMAGE_LATEST
+        config.datadog_activate_sleep = True
         datadog = DataDog(config)
 
         datadog_spec = app_spec.datadog._replace(
@@ -156,3 +157,17 @@ class TestDataDog(object):
         }
 
         assert expected == deployment.as_dict()["spec"]["template"]["spec"]["containers"][-1]["lifecycle"]
+
+    def test_does_not_add_lifecycle_when_pre_stop_delay_is_set_and_sleep_is_not_active(self, config, app_spec, deployment):
+        config.datadog_container_image = CONTAINER_IMAGE_LATEST
+        config.datadog_activate_sleep = False
+        datadog = DataDog(config)
+
+        datadog_spec = app_spec.datadog._replace(
+            enabled=True
+        )
+        app_spec = app_spec._replace(datadog=datadog_spec)
+
+        datadog.apply(deployment, app_spec, False, 5)
+
+        assert False == ("lifecycle" in deployment.as_dict()["spec"]["template"]["spec"]["containers"][-1])

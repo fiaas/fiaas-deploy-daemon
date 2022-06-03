c6b482a71475fff9765018f30319fece8351a4b5
Xavi León <xavi.leon@adevinta.com>
2022-06-03 15:52:09 +0200

    add feature back (test should pass)

--- fiaas_deploy_daemon/specs/v3/factory.py
+++ fiaas_deploy_daemon/specs/v3/factory.py
@@ -77,6 +77,7 @@ class Factory(BaseFactory):
     def _autoscaler_spec(replicas_lookup):
         maximum = replicas_lookup["maximum"]
         minimum = replicas_lookup["minimum"]
+        minimum = maximum if maximum < minimum else minimum
         enabled = minimum != maximum
         cpu_threshold_percentage = replicas_lookup["cpu_threshold_percentage"]
         return AutoscalerSpec(

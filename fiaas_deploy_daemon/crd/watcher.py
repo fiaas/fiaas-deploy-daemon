1e5cbc573d2ec6cd46ee6d1f9cf3658a1bca1151
Mika Hjalmarsson <mika.hjalmarsson@schibsted.com>
2023-10-30 15:33:34 +0100

    Refactor

--- fiaas_deploy_daemon/crd/watcher.py
+++ fiaas_deploy_daemon/crd/watcher.py
@@ -23,7 +23,7 @@ from k8s.watcher import Watcher
 from yaml import YAMLError
 
 from .status import create_name
-from .types import FiaasApplication, FiaasApplicationStatus, FiaasApplicationStatusInline
+from .types import FiaasApplication, FiaasApplicationStatus
 from ..base_thread import DaemonThread
 from ..deployer import DeployerEvent
 from ..log_extras import set_extras
@@ -72,11 +72,10 @@ class CrdWatcher(DaemonThread):
     # don't deploy if it's a status update
     def _skip_status_event(self, application):
         app_name = application.spec.application
-        namespace = application.metadata.namespace
         deployment_id = application.metadata.labels["fiaas/deployment_id"]
-        status = FiaasApplicationStatusInline.get(app_name, namespace)
-        generation = int(status.metadata.generation)
-        observed_generation = int(status.status.observedGeneration)
+        app_status = application
+        generation = app_status.metadata.generation
+        observed_generation = app_status.status.observedGeneration
         if observed_generation == generation:
             LOG.debug("Event created from status update %s for app %s", deployment_id, app_name)
             return True
@@ -84,7 +83,7 @@ class CrdWatcher(DaemonThread):
 
     def _deploy(self, application):
         if self._skip_status_event(application):
-            return 
+            return
         app_name = application.spec.application
         LOG.debug("Deploying %s", app_name)
         try:

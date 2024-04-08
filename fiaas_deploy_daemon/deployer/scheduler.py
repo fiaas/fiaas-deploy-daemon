e5a4636b49b6879fa5198b446b29c337d1a60b3a
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2024-04-08 08:57:16 +0200

    Handle exception from task (ReadyCheck)
    ReadyChecks calls the Kubernetes API to check Deployment status, and to
    update Application and ApplicationStatus resources. If any of these
    requests fail with e.g. a ClientError, the scheduler thread will exit
    and fiaas-deploy-daemon will be restarted because the liveness probe
    fails. If this failure mode is triggered by a non-recoverable error,
    such as the resource is too large, this can cause disruption to deploys,
    since fiaas-deploy-daemon would restart every time it tries to handle
    the Application triggering the error. Log and drop exceptions from
    tasks to avoid this situation. The failed task is also dropped, so
    deployment will be stuck with RUNNING status, and should be redeployed
    if fiaas-deploy-daemon restarts.

--- fiaas_deploy_daemon/deployer/scheduler.py
+++ fiaas_deploy_daemon/deployer/scheduler.py
@@ -14,12 +14,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import logging
 import time
 from queue import PriorityQueue
 from time import monotonic as time_monotonic
 from typing import Callable
 
+from k8s.client import K8sClientException
+from requests.exceptions import RetryError
+
 from ..base_thread import DaemonThread
+
+LOG = logging.getLogger(__name__)
 
 
 class Scheduler(DaemonThread):
@@ -29,15 +35,25 @@ class Scheduler(DaemonThread):
         self._time_func: Callable[[], float] = time_func
         self._delay_func: Callable[[float], None] = delay_func
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, *args, run_forever=True, **kwargs):
         while True:
             execute_at, task = self._tasks.get()
             if self._time_func() >= execute_at:
-                if task():
-                    self.add(task, 10)
+                try:
+                    if task():
+                        self.add(task, 10)
+                except (K8sClientException, RetryError):
+                    # K8sClientException: any unhandled server or client error (non-200 responses).
+                    # RetryError: request which received server error (e.g. 409 or 5xx response) was retried, and
+                    # exponential retries were exhausted.
+                    LOG.exception("Error while processing task")
             else:
                 self.add(task)
             self._delay_func(1)
+            # the run_forever parameter is only to enable testing
+            if not run_forever:
+                LOG.warning("breaking task processing loop because run_forever=%s", run_forever)
+                break
 
     def add(self, task: Callable[[], bool], delay=1):
         execute_at = self._time_func() + delay

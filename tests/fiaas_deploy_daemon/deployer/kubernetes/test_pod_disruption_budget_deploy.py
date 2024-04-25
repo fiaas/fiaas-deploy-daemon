cd6960ce97d947caaa11d893a37eb3087cb6fa98
Eloy Maillo <14107036+herodes1991@users.noreply.github.com>
2024-04-25 14:35:22 +0200

    Do not create PDBs for deployments with 0 replicas (#242)
    * Update pod_disruption_budget.py
    
    * Add tests for zero replicas
    
    ---------
    
    Co-authored-by: herodes1991 <eloy.maillo@Eloys-MacBook-Pro.local>

--- tests/fiaas_deploy_daemon/deployer/kubernetes/test_pod_disruption_budget_deploy.py
+++ tests/fiaas_deploy_daemon/deployer/kubernetes/test_pod_disruption_budget_deploy.py
@@ -74,6 +74,14 @@ class TestPodDisruptionBudgetDeployer(object):
         delete.assert_called_with(PDB_API + app_spec.name)
         pytest.helpers.assert_no_calls(post)
 
+    def test_zero_replicas_no_pdb_gives_no_post(self, deployer: PodDisruptionBudgetDeployer, delete, post, app_spec):
+        app_spec = app_spec._replace(
+            autoscaler=AutoscalerSpec(enabled=True, min_replicas=0, max_replicas=0, cpu_threshold_percentage=50)
+        )
+        deployer.deploy(app_spec, SELECTOR, LABELS)
+        delete.assert_called_with(PDB_API + app_spec.name)
+        pytest.helpers.assert_no_calls(post)
+
     def test_no_pdb_gives_no_put(self, deployer, delete, put, app_spec):
         app_spec = app_spec._replace(
             autoscaler=AutoscalerSpec(enabled=True, min_replicas=1, max_replicas=1, cpu_threshold_percentage=50)
